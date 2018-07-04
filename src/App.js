d7180ec1dc685a17a0cc3e314f74d9f9e9842ad7
Matsumoto Toshiya <toshibowie86@gmail.com>
2018-07-04 18:52:51 +0900

    second commit

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import web3 from './web3';
+import lottery from './lottery';
+
+class App extends Component {
+
+  state = {
+    manager: '',
+    players: [],
+    balance: '',
+    value: ''
+  }
+
+  async componentDidMount() {
+    const manager = await lottery.methods.manager().call();
+
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
+
+
+    this.setState({ manager, players, balance });
+
+  }
+
+  onSubmit = async (event) => {
+    event.preventDefalut();
+    const accounts = await web3.eth.getAccounts();
+
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei(this.state.value, 'ether')
+    });
+
+
+  };
+
+  render() {
+    web3.eth.getAccounts().then(console.log);
+    return (
+      <div>
+      <h2>Lottery Contract</h2>
+      <p> This contract is managed by { this.state.manager }
+          There are currently { this.state.players.length } people enter and
+          competing to { web3.utils.fromWei(this.state.balance, 'ether') } win ether !
+      </p>
+
+      <hr />
+      <form onSubmit={this.onSubmit}>
+        <h4>want to try your luck ? </h4>
+        <div>
+          <label>Amout of ether to enter</label>
+          <input
+            value={this.state.value}
+            onChange={event => this.setState({value: event.target.value })}
+          />
+        </div>
+        <button>Enter</button>
+      </form>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/lottery.js
@@ -0,0 +1,7 @@
+import web3 from './web3';
+
+const address = '0x98E13B25AD780Ef0781b6394A7358281444d9029';
+
+const abi = [{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}];
+
+export default new web3.eth.Contract(abi, address);
